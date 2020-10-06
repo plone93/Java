826d67f4b86a04cab1b24caba46d5e67c1422518
plone931206 <plone931206@gmail.com>
2020-10-06 22:00:44 +0900

    junittest

---
+++ SpringStudy/src/test/java/test/com/example/demo/controller/CalcuratorTest.java
@@ -0,0 +1,29 @@
+package test.com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcuratorTest {
+
+	@Test
+	void testPlus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMinus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTimes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivid() {
+		fail("Not yet implemented");
+	}
+
+}
